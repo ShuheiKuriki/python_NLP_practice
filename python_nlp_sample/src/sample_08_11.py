fe2fd7bb9687bcc61cba88fe2d146f6db349ce27
ShuheiKuriki <shukuri.7336.8@gmail.com>
2019-12-20 14:12:12 +0900

    revert
    なぜかファイルが二つずつあるので元に戻したい

--- python_nlp_sample/src/sample_08_11.py
+++
@@ -1,8 +0,0 @@
-import json
-
-import wordnetknowledge
-
-if __name__ == '__main__':
-    text = '幸福'
-    results = wordnetknowledge.get_hypernym(text)
-    print(json.dumps(results, indent=4, ensure_ascii=False))
--- python_nlp_sample/src/sample_08_13.py
+++
@@ -1,7 +0,0 @@
-import json
-
-import word2vec
-
-if __name__ == '__main__':
-    synonyms = word2vec.get_synonyms('アメリカ')
-    print(json.dumps(synonyms, indent=4, ensure_ascii=False))
